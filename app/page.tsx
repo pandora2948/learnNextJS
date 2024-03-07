5556e26f0665e148e04c14dcc59c87e4d9d4d7f7
백동재 <baekdongjae@baegdongjaeui-MacBookPro.local>
2024-03-07 17:08:05 +0900

    fix: dashboard layout fixed

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
             <Image src="/hero-desktop.png" width={1000} height={760} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop version"></Image>
-            <Image src="/hero-mobile.png" width={560} height={620} className="block md:block"></Image>
+            <Image src="/hero-mobile.png" width={560} height={620} className="block md:block" alt="Screenshot of the dashboard project showing mobile version"></Image>
         </div>
       </div>
     </main>
